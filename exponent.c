a07cb96f2219075226e0d1df048c9577963218bb
ShatakshiMishra <73740877+ShatakshiMishra@users.noreply.github.com>
2021-08-05 11:44:37 +0530

    Update exponent.c

--- exponent.c
+++ exponent.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-int poww(int m,int n)
-{
-    if(n==0)
-    return 1;
-    else
-    return poww(m,n-1)*m;
-}
 int pow1(int m,int n)
 {
     if(n==0)
     return 1;
+    else
+    return pow1(m,n-1)*m;
+}
+int pow2(int m,int n)
+{
+    if(n==0)
+    return 1;
     if(n%2==0)
-    return pow1(m*m,n/2);
-    return m*pow1(m*n,(n-1)/2);
+    return pow2(m*m,n/2);
+    return m*pow2(m*n,(n-1)/2);
 }
 int main() {
     // Write C code here
     int p;
    p= pow1(2,8);
    printf("%d  ",p);
-   p=poww(2,9);
+   p=pow2(2,9);
     printf("%d",p);
     return 0;
 }
